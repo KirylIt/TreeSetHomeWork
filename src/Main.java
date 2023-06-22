9a960cf941b0450bc654737fa69dc0305e6b465b
Kiryl Kunavich <kirill99hockey@gmail.com>
2023-06-22 23:55:22 +0300

    1.0

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.util.TreeSet;
+
+public class Main {
+    public static void main(String[] args) {
+
+        TreeSet ts = new TreeSet();
+        ts.add("dog");
+        ts.add("cat");
+        ts.add("duck");
+        ts.add("book");
+        ts.add("clock");
+        ts.add("soup");
+        ts.add("bad");
+        ts.add("mather");
+        ts.add("brother");
+        ts.add("apple");
+        ts.add("car");
+        ts.add("rabbit");
+        ts.add("oil");
+        ts.add("coil");
+        ts.add("soil");
+        ts.add("salt");
+        ts.add("sugar");
+        ts.add("paper");
+        ts.add("pencil");
+        ts.add("birth");
+        System.out.println(ts);
+        System.out.println("Size: " + ts.size());
+
+    }
+}
